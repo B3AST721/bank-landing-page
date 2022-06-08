f4819e07313e52c818bf471421cc1ce69d2ec022
Hansel Warrior <jaheelwarr@gmail.com>
2022-06-08 09:38:37 -0600

    Created modules for intro page

---
+++ pages/components/Intro.js
@@ -0,0 +1,10 @@
+import '/home/jaheel/bank-landing-page/pages/components/Intro.module.css';
+import IntroPictures from './IntroPictures';
+
+function Intro() {
+  return (
+    <div>Intro</div>
+  )
+}
+
+export default Intro
---
+++ pages/components/IntroPictures.js
@@ -0,0 +1,9 @@
+import '/home/jaheel/bank-landing-page/pages/components/IntroPictures.module.css';
+
+function IntroPictures() {
+  return (
+    <div>IntroPictures</div>
+  )
+}
+
+export default IntroPictures
